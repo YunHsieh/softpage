81e1e76bb3608372a19712c111050cfa094dbcf2
yun <kilicapeto@gmail.com>
2022-11-04 01:20:13 +0800

    add: nextjs initial

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = {
+  ...nextConfig,
+  trailingSlash: true,
+  // after 12.3 versions.
+  images: {
+    unoptimized: true
+  },
+}
