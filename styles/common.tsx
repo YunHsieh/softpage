ac05de375ede4d44dbfb402b82df3e01e938e8af
yun <kilicapeto@gmail.com>
2022-12-01 18:13:50 +0800

    add: inhert css usage for editable in div

---
+++ styles/common.tsx
@@ -0,0 +1,10 @@
+import styled, { css } from 'styled-components'
+
+
+export const ContentEditable = css`
+    -webkit-user-modify: read-write;
+    -moz-user-modify: read-write;
+    user-modify: read-write;
+    border: 0; 
+    outline: 0;
+`;
