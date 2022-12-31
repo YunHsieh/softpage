f11b3949688ffae771b162dac96a0070b9bae2f4
yun <kilicapeto@gmail.com>
2022-12-31 20:35:07 +0800

    add: change to dark mode

--- styles/card.tsx
+++ styles/card.tsx
@@ -2,5 +2,7 @@ import styled from 'styled-components'
 
 
 export const EachCardEdge = styled.div`
-    padding-top: 7px;
+    padding-top: 2vh;
+    padding-right: 1.5vh;
+    padding-left: 1.5vh;
 `
