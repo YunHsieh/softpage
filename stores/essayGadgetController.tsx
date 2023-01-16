805a4d89fd3a689a4226285f28b1fcd2c31214ad
yun <kilicapeto@gmail.com>
2023-01-17 03:32:40 +0800

    add: move gadget controller  to the store

---
+++ stores/essayGadgetController.tsx
@@ -0,0 +1,31 @@
+import { AppState } from "store";
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface DataState {
+    isCompared: boolean;
+    isParsed: boolean;
+}
+
+const initialState = {
+    isCompared: false,
+    isParsed: false
+}
+
+export const essayGadGetSwtichers = createSlice({
+   
+    name: 'essayGadGetSwtichers',
+    initialState,
+    reducers: {
+        setGadgetReset: (state: DataState, _action) => {
+            return initialState
+        },
+        setGadgetState: (state: DataState, action) => {
+            return {...state, ...action.payload}
+        }
+    },
+});
+
+export const { setGadgetState, setGadgetReset } = essayGadGetSwtichers.actions;
+
+export const selectGadGetSwtichers = (state: AppState) => state.essayGadGetSwtichers;
--- stores/index.tsx
+++ stores/index.tsx
@@ -1,8 +1,10 @@
 import { authSlice } from "./authSlice";
 import { essaySlice } from "./softEssay";
+import { essayGadGetSwtichers } from "./essayGadgetController";
 
 // register reduer
 export const reducers = {
     [authSlice.name]: authSlice.reducer,
     [essaySlice.name]: essaySlice.reducer,
+    [essayGadGetSwtichers.name]: essayGadGetSwtichers.reducer,
 }
