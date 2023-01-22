d6750b0393b44a3dfd2688bb93fe8786b330e35c
yun <kilicapeto@gmail.com>
2023-01-22 20:32:32 +0800

    add: save component

--- components/units/gadgets.tsx
+++ components/units/gadgets.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import SaveIcon from 'public/save.png'
 import CompareIcon from 'public/compare.png'
 import { connect } from 'react-redux';
-import { GadgetContainer, GadgetItems, MyIcon, ToolsContainer } from 'styles/mainpageTools';
+import { GadgetUnit, GadgetItems, MyIcon, SavedContainer, GadgetComponents, GadgetsContainer, SavedCommentText, SavedDescText, SavedButton } from 'styles/mainpageTools';
 import { setGadgetState } from 'stores/essayGadgetController';
 import { resetCurrentEssay } from 'stores/softEssay'
+import { Input } from '@mui/material';
 
 interface GadgetProps {
     setGadgetState: any;
@@ -12,6 +13,7 @@ interface GadgetProps {
     currentEssay: any;
     isCompared: boolean;
     isParsed: boolean;
+    isSaved: boolean;
 }
 
 interface GadgetState {
@@ -26,40 +28,73 @@ class GadgetFunction extends React.Component<GadgetProps, GadgetState> {
 
     changeParseState = () => {
         if (this.props.currentEssay.id) {
-            this.props.setGadgetState({'isParsed': !this.props.isParsed})
+            this.props.setGadgetState({
+                isParsed: !this.props.isParsed,
+                isSaved: false,
+            })
             this.props.resetCurrentEssay()
         }
     }
-
+    
     changeCompareState = () => {
         if (this.props.currentEssay.id) {
-            this.props.setGadgetState({'isCompared': !this.props.isCompared})
+            this.props.setGadgetState({
+                isCompared: !this.props.isCompared,
+                isSaved: false,
+            })
         }
+    }
+
+    changeSaved = () => {
+        if (this.props.currentEssay.id) {
+            this.props.setGadgetState({'isSaved': !this.props.isSaved})
+        }
+    }
+
+    handleSaveClick = () => {
+        this.props.setGadgetState({'isSaved': false})
     }
 
     render() {
         return (
-            <ToolsContainer>
-                <GadgetContainer onClick={() => this.changeParseState()}>
-                    <GadgetItems>{'{}'}</GadgetItems>
-                </GadgetContainer>
-                <GadgetContainer>
-                    <MyIcon 
-                        src={SaveIcon} 
-                        draggable="false" 
-                        alt="Save your essay"
-                    />
-                </GadgetContainer>
-                <GadgetContainer 
-                    onClick={() => this.changeCompareState()}
-                >
-                    <MyIcon 
-                        src={CompareIcon} 
-                        draggable="false" 
-                        alt="Compare your essay" 
-                    />
-                </GadgetContainer>
-            </ToolsContainer>
+            <GadgetsContainer>
+                <GadgetComponents>
+                    <GadgetUnit onClick={() => this.changeParseState()}>
+                        <GadgetItems>{'{}'}</GadgetItems>
+                    </GadgetUnit>
+                    <GadgetUnit
+                        onClick={() => this.changeSaved()}
+                    >
+                        <MyIcon 
+                            src={SaveIcon} 
+                            draggable="false" 
+                            alt="Save your essay"
+                        />
+                    </GadgetUnit>
+                    <GadgetUnit 
+                        onClick={() => this.changeCompareState()}
+                    >
+                        <MyIcon 
+                            src={CompareIcon} 
+                            draggable="false" 
+                            alt="Compare your essay" 
+                        />
+                    </GadgetUnit>
+                </GadgetComponents>
+                {
+                    this.props.isSaved && 
+                    <SavedContainer>
+                        <SavedCommentText
+                            onKeyDown={(event: any) => {
+                                event.keyCode === 13 && event.preventDefault();
+                            }}
+                        >
+                        </SavedCommentText>
+                        <SavedDescText></SavedDescText>
+                        <SavedButton onClick={this.handleSaveClick}> Save </SavedButton>
+                    </SavedContainer>
+                }
+            </GadgetsContainer>
         )
     }
 }
@@ -68,6 +103,7 @@ const mapStateToProps = (state: any) => ({
     currentEssay: state.essays.currentEssay,
     isCompared: state.essayGadGetSwtichers.isCompared,
     isParsed: state.essayGadGetSwtichers.isParsed,
+    isSaved: state.essayGadGetSwtichers.isSaved,
 });
 
 
